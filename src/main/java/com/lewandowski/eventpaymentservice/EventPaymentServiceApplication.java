e396745c51938ee6abd013e22fdbc7dfa398e02f
milewandowski <m.21lewandowski@gmail.com>
2023-11-20 15:51:48 +0100

    Initial commit

---
+++ src/main/java/com/lewandowski/eventpaymentservice/EventPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lewandowski.eventpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lewandowski/eventpaymentservice/EventPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewandowski.eventpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
